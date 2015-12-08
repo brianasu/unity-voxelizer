1d72a189478368fad038ceaf206c62b35fc49966
Brian <brianasu@192-168-1-7.tpgi.com.au>
2015-12-08 22:16:12 +1100

    initial commit

---
+++ Assets/Voxeliser/TriAABBOverlap.cs
@@ -0,0 +1,281 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TriAABBOverlap
+{
+	public struct Triangle
+	{
+		public Vector3 vertA;
+		public Vector3 vertB;
+		public Vector3 vertC;
+		public Vector3 normal;
+		public Bounds bound;
+		public bool didHit;
+	}
+	
+	public struct BoundHierarchy
+	{
+		public Bounds bound;
+		public BoundHierarchy[] subBounds;
+		public Triangle triList;
+	}
+
+	// cache everything
+	private static Vector3 vmin = Vector3.zero;
+	private static Vector3 vmax = Vector3.zero;
+	private static Vector3 vertA = Vector3.zero;
+	private static Vector3 vertB = Vector3.zero;
+	private static Vector3 vertC = Vector3.zero;
+	private static Vector3 edgeA = Vector3.zero;
+	private static Vector3 edgeB = Vector3.zero;
+	private static Vector3 edgeC = Vector3.zero;
+	
+	private static bool PlaneBoxOverlap(Vector3 normal, Vector3 vert, Vector3 maxbox)
+	{
+		vmin.x = vmin.y = vmin.z = 0; 
+		vmax.x = vmax.y = vmax.z = 0;
+
+		// unroll loop
+		var vX = vert.x; 
+		var vY = vert.y; 
+		var vZ = vert.z; 
+		var maxX = maxbox.x;
+		var maxY = maxbox.y;
+		var maxZ = maxbox.z;
+
+		if(normal.x > 0.0f)
+		{
+			vmin.x = -maxX - vX;
+			vmax.x =  maxX - vX;
+		}
+		else
+		{
+			vmin.x =  maxX - vX;
+			vmax.x = -maxX - vX;
+		}
+	
+		if(normal.y > 0.0f)
+		{
+			vmin.y = -maxY - vY;
+			vmax.y =  maxY - vY;
+		}
+		else
+		{
+			vmin.y =  maxY - vY;
+			vmax.y = -maxY - vY;
+		}
+
+		if(normal.z > 0.0f)
+		{
+			vmin.z = -maxZ - vZ;
+			vmax.z =  maxZ - vZ;
+		}
+		else
+		{
+			vmin.z =  maxZ - vZ;
+			vmax.z = -maxZ - vZ;
+		}
+		
+		if(Vector3.Dot(normal, vmin) > 0.0f)
+		{ 
+			return false;
+		}	
+		
+		if(Vector3.Dot(normal, vmax) >= 0.0f)
+		{ 
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private static void FindMinMax(float x0, float x1, float x2, out float min, out float max)
+	{
+		min = (x0 < x1) ? ((x0 < x2) ? x0 : x2) : ((x1 < x2) ? x1 : x2);
+		max = (x0 > x1) ? ((x0 > x2) ? x0 : x2) : ((x1 > x2) ? x1 : x2);
+	}
+
+	public static bool Project(
+		float ea1, float ea2, 
+		float v1a1, float v1a2, 
+		float v2a1, float v2a2, float rad)
+	{
+		var p0 = ea1 * v1a2 - ea2 * v1a1;			     
+		var p2 = ea1 * v2a2 - ea2 * v2a1;	
+		var min = p0 < p2 ? p0 : p2;
+		var max = p0 > p2 ? p0 : p2;
+		if(min > rad || max < -rad)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public static bool Project(Vector3 vert1, Vector3 vert2, Vector3 edge, int axis1, int axis2, float rad)
+	{
+		var p0 = edge[axis1] * vert1[axis2] - edge[axis2] * vert1[axis1];			     
+		var p2 = edge[axis1] * vert2[axis2] - edge[axis2] * vert2[axis1];	
+		var min = p0 < p2 ? p0 : p2;
+		var max = p0 > p2 ? p0 : p2;
+		if(min > rad || max < -rad)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	// faster than Mathf.Abs
+	public static float Abs (float val)
+	{
+		return val < 0 ? -val : val;
+	}
+
+	public static bool Check(Bounds bounds, Triangle triangle)
+	{
+		var boundsCentre = bounds.center;
+		var boundsExtents = bounds.extents;
+
+		vertA.x = triangle.vertA.x - boundsCentre.x;
+		vertA.y = triangle.vertA.y - boundsCentre.y;
+		vertA.z = triangle.vertA.z - boundsCentre.z;
+
+		vertB.x = triangle.vertB.x - boundsCentre.x;
+		vertB.y = triangle.vertB.y - boundsCentre.y;
+		vertB.z = triangle.vertB.z - boundsCentre.z;
+
+		vertC.x = triangle.vertC.x - boundsCentre.x;
+		vertC.y = triangle.vertC.y - boundsCentre.y;
+		vertC.z = triangle.vertC.z - boundsCentre.z;
+
+		// overlap in the {x,y,z}-directions 
+		// find min, max of the triangle each direction, and test for overlap in 
+		// that direction -- this is equivalent to testing a minimal AABB around 
+		// the triangle against the AABB 
+		float min, max;
+		// test in X-direction 
+		FindMinMax(vertA.x, vertB.x, vertC.x, out min, out max);
+		if(min > boundsExtents.x || max < -boundsExtents.x)
+		{
+			return false;
+		}
+		// test in Y-direction 
+		FindMinMax(vertA.y, vertB.y, vertC.y, out min, out max);
+		if(min > boundsExtents.y || max < -boundsExtents.y)
+		{
+			return false;
+		}
+		// test in Z-direction 
+		FindMinMax(vertA.z, vertB.z, vertC.z, out min, out max);
+		if(min > boundsExtents.z || max < -boundsExtents.z)
+		{
+			return false;
+		}
+
+		edgeA.x = vertB.x - vertA.x;
+		edgeA.y = vertB.y - vertA.y;
+		edgeA.z = vertB.z - vertA.z;
+
+		edgeB.x = vertC.x - vertB.x;
+		edgeB.y = vertC.y - vertB.y;
+		edgeB.z = vertC.z - vertB.z;
+
+		edgeC.x = vertA.x - vertC.x;
+		edgeC.y = vertA.y - vertC.y;
+		edgeC.z = vertA.z - vertC.z;
+
+		// Project the points on to the planes formed by the aabb normals
+		var fex = Abs(edgeA.x);
+		var fey = Abs(edgeA.y);
+		var fez = Abs(edgeA.z);
+		if(Project(
+			edgeA.z, edgeA.y, 
+			vertA.z, vertA.y, 
+			vertC.z, vertC.y, 
+			fez * boundsExtents.y + fey * boundsExtents.z))
+		{
+			return false;
+		}
+		if(Project(
+			-edgeA.z, -edgeA.x, 
+			vertA.z, vertA.x, 
+			vertC.z, vertC.x, 
+			fez * boundsExtents.x + fex * boundsExtents.z))
+		{
+			return false;
+		}
+		if(Project(
+			edgeA.y, edgeA.x, 
+			vertB.y, vertB.x, 
+			vertC.y, vertC.x, 
+			fey * boundsExtents.x + fex * boundsExtents.y))
+		{
+			return false;
+		}
+		
+		fex = Abs(edgeB.x);
+		fey = Abs(edgeB.y);
+		fez = Abs(edgeB.z);
+		if(Project(
+			edgeB.z, edgeB.y, 
+			vertA.z, vertA.y, 
+			vertC.z, vertC.y, 
+			fez * boundsExtents.y + fey * boundsExtents.z))
+		{
+			return false;
+		}
+		if(Project(
+			-edgeB.z, -edgeB.x, 
+			vertA.z, vertA.x, 
+			vertC.z, vertC.x, 
+			fez * boundsExtents.x + fex * boundsExtents.z))
+		{
+			return false;
+		}
+		if(Project(
+			edgeB.y, edgeB.x, 
+			vertA.y, vertA.x, 
+			vertB.y, vertB.x, 
+			fey * boundsExtents.x + fex * boundsExtents.y))
+		{
+			return false;
+		}
+		
+		fex = Abs(edgeC.x);
+		fey = Abs(edgeC.y);
+		fez = Abs(edgeC.z);
+
+		if(Project(
+			edgeC.z, edgeC.y, 
+			vertA.z, vertA.y, 
+			vertB.z, vertB.y, 
+			fez * boundsExtents.y + fey * boundsExtents.z))
+		{
+			return false;
+		}
+
+		if(Project(
+			-edgeC.z, -edgeC.x, 
+			vertA.z, vertA.x, 
+			vertB.z, vertB.x, 
+			fez * boundsExtents.x + fex * boundsExtents.z))
+		{
+			return false;
+		}
+
+		if(Project(
+			edgeC.y, edgeC.x, 
+			vertB.y, vertB.x, 
+			vertC.y, vertC.x, 
+			fey * boundsExtents.x + fex * boundsExtents.y))
+		{
+			return false;
+		}
+
+		// if the box intersects the plane of the triangle  compute plane equation of triangle: normal * x + d = 0 
+		if(!PlaneBoxOverlap(triangle.normal, vertA, boundsExtents))
+		{
+			return false;
+		}
+		return true; 
+	}
+}
---
+++ Assets/Voxeliser/VoxeliseScene.cs
@@ -0,0 +1,260 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+using BoundHierarchy = TriAABBOverlap.BoundHierarchy;
+using Triangle = TriAABBOverlap.Triangle;
+
+public class VoxeliseScene : MonoBehaviour
+{
+	[Header("Click the settings icon for more options")]
+	[Header("Version 1.0 Brian Su")]
+	[SerializeField]
+	private GameObject _voxelModel;
+	[SerializeField]
+	[Header("Keep this value low 8 = 8^3 = 512, 16^3 = 4096 voxels")]
+	private int _xDensity = 8;
+	[SerializeField]
+	private int _yDensity = 8;
+	[SerializeField]
+	private int _zDensitySize = 8;
+	[Header("Area where to voxelise")]
+	[SerializeField]
+	private Bounds bounds = new Bounds(Vector3.zero, Vector3.one * 50);
+	[Header("Meshrenderers under this will be processed.")]
+	[SerializeField]
+	private Transform root;
+
+	private bool[][][] _voxelMap;
+	private MeshFilter[] _meshFilter;
+	private BoundHierarchy _rootNode;
+
+	private void GenerateVoxelData(BoundHierarchy rootNode)
+	{
+		var gridCubeSize = new Vector3(
+			bounds.size.x / _xDensity,
+			bounds.size.y / _yDensity,
+			bounds.size.z / _zDensitySize);
+		var worldCentre = bounds.min + gridCubeSize / 2;
+		var objectLevelBounds = rootNode.subBounds;
+		var cachedGridBounds = new Bounds(Vector3.zero, gridCubeSize);
+		var cachedVec = Vector3.zero;
+
+#if UNITY_EDITOR
+		EditorUtility.ClearProgressBar();
+		var pointsProcessed = 0;
+		var totalPoints = (float)(_xDensity * _yDensity * _zDensitySize);
+#endif
+
+		for(int x = 0; x < _xDensity; x++)
+		{
+			for(int y = 0; y < _yDensity; y++)
+			{
+				for(int z = 0; z < _zDensitySize; z++)
+				{
+					#if UNITY_EDITOR
+					pointsProcessed++;
+					if (Application.isEditor && !Application.isPlaying)
+					{
+						if (pointsProcessed % 2000 == 0)
+						{
+							if(EditorUtility.DisplayCancelableProgressBar("Voxelising", "Voxelising", (float)pointsProcessed / totalPoints))
+							{
+								EditorUtility.ClearProgressBar();
+								return;
+							}
+						}
+					}
+					#endif
+
+					var didFind = false;
+					for(int objectCnt = 0; objectCnt < objectLevelBounds.Length; objectCnt++)
+					{
+						cachedVec.x = x * gridCubeSize.x + worldCentre.x;
+						cachedVec.y = y * gridCubeSize.y + worldCentre.y;
+						cachedVec.z = z * gridCubeSize.z + worldCentre.z;
+						cachedGridBounds.center = cachedVec;
+
+						if(cachedGridBounds.Intersects(objectLevelBounds[objectCnt].bound))
+						{
+							var triBounds = objectLevelBounds[objectCnt].subBounds;
+							for(int triCnt = 0; triCnt < triBounds.Length; triCnt++)
+							{
+								var triangle = triBounds[triCnt].triList;
+								if(TriAABBOverlap.Check(cachedGridBounds, triangle))
+								{
+								
+									_voxelMap[x][y][z] = true;
+									didFind = true;
+									break;
+								}
+							}
+							if(didFind)
+							{
+								break;
+							}
+						}
+						if(didFind)
+						{
+							break;
+						}
+					}
+				}
+			}
+		}
+
+#if UNITY_EDITOR
+		EditorUtility.ClearProgressBar();
+#endif
+	}
+
+	[ContextMenu("Run and create voxels")]
+	private void RunAndCreate ()
+	{
+		Run();
+
+		var gridCubeSize = new Vector3(
+			bounds.size.x / _xDensity,
+			bounds.size.y / _yDensity,
+			bounds.size.z / _zDensitySize);
+		var worldCentre = bounds.min + gridCubeSize / 2;
+		var voxelRoot = new GameObject("Voxel Root");
+		var rootTransform = voxelRoot.transform;
+
+		for(int x = 0; x < _xDensity; x++)
+		{
+			for(int y = 0; y < _yDensity; y++)
+			{
+				for(int z = 0; z < _zDensitySize; z++)
+				{
+					if (_voxelMap[x][y][z])
+					{
+						var go = Instantiate(_voxelModel, new Vector3(
+							x * gridCubeSize.x,
+							y * gridCubeSize.y,
+							z * gridCubeSize.z) + worldCentre, Quaternion.identity) as GameObject;
+						go.transform.localScale = gridCubeSize;
+						go.transform.SetParent(rootTransform, true);
+					}
+				}
+			}
+		}
+	}
+
+	[ContextMenu("Debug run")]
+	private void Run()
+	{
+		_meshFilter = root.GetComponentsInChildren<MeshFilter>();
+
+		var objectBounds = new List<BoundHierarchy>();
+		foreach (var filter in _meshFilter)
+		{
+			var mesh = filter.sharedMesh;
+			var vertices = mesh.vertices;
+			var tris = mesh.triangles;
+			var triangleBounds = new List<BoundHierarchy>();
+			for(int i = 0; i < tris.Length; i += 3)
+			{
+				var vert1 = vertices[tris[i + 0]];
+				var vert2 = vertices[tris[i + 1]];
+				var vert3 = vertices[tris[i + 2]];
+				vert1 = filter.transform.TransformPoint(vert1);
+				vert2 = filter.transform.TransformPoint(vert2);
+				vert3 = filter.transform.TransformPoint(vert3);
+
+				var u = vert2 - vert3;
+				var v = vert3 - vert1;
+				var triNormal = Vector3.Cross(u, v);
+				triNormal = triNormal.normalized;
+				
+				var triBounds = new Bounds(vert1, Vector3.zero);
+				triBounds.Encapsulate(vert2);
+				triBounds.Encapsulate(vert3);
+
+				var tri = new Triangle {
+						vertA = vert1,
+						vertB = vert2,
+						vertC = vert3,
+						normal = triNormal,
+						bound = triBounds,
+					};
+
+				triangleBounds.Add(new BoundHierarchy() { 
+					bound = triBounds, 
+					subBounds = null, 
+					triList = tri 
+				});
+			}
+
+			objectBounds.Add(new BoundHierarchy() { 
+				bound = filter.GetComponent<Renderer>().bounds,  
+				subBounds = triangleBounds.ToArray() 
+			});
+		}
+
+		_rootNode = new BoundHierarchy() { 
+			bound = bounds, 
+			subBounds = objectBounds.ToArray() 
+		};
+
+		GenerateBlockArray ();
+		GenerateVoxelData (_rootNode);
+	}
+
+	private void GenerateBlockArray ()
+	{
+		_voxelMap = new bool[_xDensity][][];
+		for (var x = 0; x < _xDensity; x++)
+		{
+			_voxelMap[x] = new bool[_yDensity][];
+			for (var y = 0; y < _yDensity; y++)
+			{
+				_voxelMap[x][y] = new bool[_zDensitySize];
+			}
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.DrawWireCube(transform.position + bounds.center, bounds.size);
+		if (_voxelMap != null)
+		{
+			var gridCubeSize = new Vector3(
+				bounds.size.x / _xDensity,
+				bounds.size.y / _yDensity,
+				bounds.size.z / _zDensitySize);
+			var worldCentre = bounds.min + gridCubeSize / 2;
+
+			if (
+				_xDensity != _voxelMap.Length ||
+				_yDensity != _voxelMap[0].Length ||
+				_zDensitySize != _voxelMap[0][0].Length)
+			{
+				_voxelMap = null;
+				return;
+			}
+			
+			for(int x = 0; x < _xDensity; x++)
+			{
+				for(int y = 0; y < _yDensity; y++)
+				{
+					for(int z = 0; z < _zDensitySize; z++)
+					{
+						if (_voxelMap[x][y][z])
+						{
+							Gizmos.DrawWireCube(new Vector3(
+								x * gridCubeSize.x,
+								y * gridCubeSize.y,
+								z * gridCubeSize.z) + worldCentre, gridCubeSize);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+			
